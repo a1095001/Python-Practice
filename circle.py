c1065e8c95b5f74c38e5c5912a8f49c69cb2bf93
a1095001 <KeymanWu0@hotmail.com>
2020-01-03 22:07:45 +0800

    完成计算圆面积程序

--- circle.py
+++ circle.py
@@ -1,8 +1,38 @@
 #计算圆的面积
 
 import math
+import unicodedata
 
 Pi = math.pi
 
 def calculate():
-    r = 
+    r = input('请输入圆的半径：\n')
+    while is_number(r) == False or float(r) <= 0:
+        print('你输入为非法值，请输入大于0的数！！！\n')
+        r = input('请输入半径')
+        pass
+    else:
+        r = float(r)
+        area = pow(r , 2)*Pi
+        print('圆的面积为：%.3f'%area)
+        input('输入回车键退出')
+        pass
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+        pass
+    except ValueError:
+        return False
+        pass
+    try:
+        i = 0
+        for i in s:
+            unicodedata.numeric(i)
+            return True
+        pass
+    except(TypeError, ValueError):
+        pass
+    return False
+calculate()
