67653c5c090ea1dbba0e99e1f758c9820952b4fa
a1095001 <KeymanWu0@hotmail.com>
2020-01-08 14:04:42 +0800

    更改单位

--- transformCeltoFha.py
+++ transformCeltoFha.py
@@ -2,4 +2,4 @@
 while 1:
     Celsius = float(input('请输入当前温度为'))
     Fharenheit = (Celsius * 1.8) + 32
-    print('{0}摄氏度等于{1}华氏度'.format(Celsius , Fharenheit))
+    print('{0}℃ 等于{1}℉'.format(Celsius , Fharenheit))
