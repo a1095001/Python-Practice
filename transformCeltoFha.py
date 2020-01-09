b9b00a28f65c4b91c2a5028e3afc66c9763323ca
a1095001 <KeymanWu0@hotmail.com>
2020-01-09 23:26:04 +0800

    完善模式选择功能

--- transformCeltoFha.py
+++ transformCeltoFha.py
@@ -5,27 +5,27 @@ from sys import exit
 
 flag = 0
 while(flag == 0):
-    print('是否要开始换算程序？\n [1]:开始\n [2]:退出')
-    mode = input()
+    print('开始换算程序，请选择模式\n [1]：℃  → ℉\n [2]：℉  → ℃')
+    mode = int(input())
     while mode == 1 or mode == 2:
         if mode == 1:
             Celsius = 0
-            Fharenheit = 0
-            Celsius = float(input('请输入当前温度为：%c℃'%Celsius))
-            Fharenheit = (Celsius * 1.8) + 32
-            print('{0}℃ 等于{1}℉'.format(Celsius , Fharenheit))
+            fahrenheit = 0
+            Celsius = float(input('请输入当前温度为：摄氏度'))
+            fahrenheit = (Celsius * 1.8) + 32
+            print('{0}℃ 等于{1}℉'.format(Celsius , fahrenheit))
+            mode = 0
             pass
         elif mode == 2:
             Celsius = 0
-            Fharenheit = 0
-            Fharenheit = float(input('请输入当前温度为：%F℉'%Fharenheit))
-            Celsius = (Fharenheit - 32)/1.8
-            print('{2}℉ 等于{3}℃'.format(Fharenheit , Celsius))
+            fahrenheit = 0
+            fahrenheit = float(input('请输入当前温度为：%F华氏度'%fahrenheit))
+            Celsius = (fahrenheit - 32)/1.8
+            print('{2}℉ 等于{3}℃'.format(fahrenheit , Celsius))
+            mode = 0
             pass
 else:
-    print('是否要退出换算？y/n?')
-    temp = input()
-    if temp == 'y':
+    if flag == 1:
         print('程序将在1S后退出')
         time.sleep(1)
         exit(0)
