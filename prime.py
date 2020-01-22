f49520cd11146844fdbdb06e5eac4ff955f17ce9
a1095001 <KeymanWu0@hotmail.com>
2020-01-22 14:21:35 +0800

    质数

---
+++ prime.py
@@ -0,0 +1,40 @@
+# Python 程序用于检测用户输入的数字是质数还是合数
+import math
+
+# 质数大于 1
+def judge_prime(num):
+    if num > 1:
+        # 找到其平方根（ √ ），减少算法时间
+        square_num = math.floor( num ** 0.5 )
+        # 查找其因子
+        for i in range(2, (square_num+1)): #将平凡根加1是为了能取到平方根那个值
+            if (num % i) == 0:
+                '''print(num, "是合数")
+                print(i, "乘于", num // i, "是", num)'''
+                return False
+        else:
+            #print(num, "是质数")
+            # 如果输入的数字小于或等于 1，不是质数
+            return True
+    else:
+        #print(num, "既不是质数，也不是合数")
+        pass
+
+# 用户输入数字
+#num = int(input("请输入一个数字: "))
+bouldery = int(input('请输入一个范围'))
+prime_list = []
+flag = False
+temp = 0
+for i in range(1 , bouldery , 1):
+    temp = i
+    flag = judge_prime(i)
+    while flag == True:
+        prime_list.append(temp)
+        flag = False
+        pass
+    else:
+        pass
+    break
+print(prime_list)
+
--- print_the_prime.py
+++ print_the_prime.py
@@ -16,6 +16,20 @@ def is_int(flag):
         pass
     return False
 
+def judge_prime(num):
+    num = float(num)
+    if num == 1:
+        return False
+    else:
+        square_num = math.floor(num ** 0.5)
+        for n in range(2,square_num + 1):    #创建一个包含2到square_num的'range'，查找因子
+            if (num % n) == 0:
+                return False
+            else:
+                return True
+            pass
+        pass
+
 bouldery = input('请输入素数表范围：')
 flag = is_int(bouldery)
 i = 0
@@ -27,10 +41,25 @@ else:
     bouldery = int(bouldery)
     while bouldery <= 1:
         bouldery = int(input('你输入的是一个负数！请重新输入一个正整数：'))
-        i = i + 1
+        i += 1
         if i == 3:
             print('你已经连续三次输入错误，程序将在1S后关闭！！！')
             time.sleep(1)
             sys.exit(0)
             pass
         pass
+    else:
+        s = 0
+        temp = 0
+        prime_number = []
+        for s in range(1 , bouldery , 1):
+            temp = s
+            list_flag = judge_prime(s)
+            while list_flag == True:
+                prime_number.append(temp)
+                list_flag = False
+                pass
+            else:
+                pass
+            pass
+        print(prime_number)
