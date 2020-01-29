879339ed63ea1c907d09aa26ef8dca8bdca69668
a1095001 <KeymanWu0@hotmail.com>
2020-01-29 23:28:44 +0800

    阶乘

---
+++ factorial.py
@@ -0,0 +1,16 @@
+#  计算阶乘
+import math
+import unicodedata
+
+#  方法一：直接调用math模块的阶乘函数
+'''
+num = int(input("请输入一个数字："))
+if num < 0:
+    print("负数没有阶乘！")
+    pass
+else:
+    print("{}! = {}".format(num , math.factorial(num)))
+'''
+
+#  方法二：
+
