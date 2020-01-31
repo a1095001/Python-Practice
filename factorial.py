50f52a5cb2a205f951027db897982adfda224c20
a1095001 <KeymanWu0@hotmail.com>
2020-01-31 13:55:00 +0800

    阶乘

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
 #  计算阶乘
 import math
 import unicodedata
-
-#  方法一：直接调用math模块的阶乘函数
 '''
+#  方法一：直接调用math模块的阶乘函数
+
 num = int(input("请输入一个数字："))
 if num < 0:
     print("负数没有阶乘！")
@@ -12,5 +12,8 @@ else:
     print("{}! = {}".format(num , math.factorial(num)))
 '''
 
-#  方法二：
+#  方法二：递归实现
+def factorial(number):
+    if number > 1:
+        return number * factorial(number - 1)
 
