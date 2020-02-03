48dfc2f4c9c36af555e94bb8ab473d8481f6a8bb
a1095001 <KeymanWu0@hotmail.com>
2020-02-03 11:55:02 +0800

    循环语句实现阶乘

--- factorial.py
+++ factorial.py
@@ -12,9 +12,31 @@ else:
     print("{}! = {}".format(num , math.factorial(num)))
 '''
 
+'''
 #  方法二：递归实现
 def factorial(number):
-    if number > 1:
-        return number * factorial(number - 1)
+    if number < 1:
+        return False
+    elif number == 1:
+        return 1
+    else:
+        return number * (number - 1)
 
 num = int(input('请输入您要计算的数：'))
+print('{}! = {}'.format(num , factorial(num)))
+'''
+
+#  方法三：循环语句
+num = int(input('请输入要计算的数字：'))
+factorial = 1
+for count in range(1 , num + 1 , 1):
+    while num < 0:
+        num = input('负数没有阶乘！请重新输入一个自然数：')
+        pass
+    if num == 1 or 0:
+        print('1! = 1')
+        break
+    elif num > 1:
+        factorial = factorial * count
+        pass
+    print('{0}! = {1}'.format(count , factorial))
