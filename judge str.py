c547c6b34b7f9ba7bfac0c984902e1f8b36a8b68
a1095001 <KeymanWu0@hotmail.com>
2020-02-08 16:36:15 +0800

    字符串判断

---
+++ judge str.py
@@ -0,0 +1,23 @@
+# 测试实例一
+print('测试实例一')
+str = 'www.baidu.com'
+print(str.isalnum()) # 判断所有字符都是数字或者字母
+print(str.isalpha()) # 判断所有字符都是字母
+print(str.isdigit()) # 判断所有字符都是数字
+print(str.islower()) # 判断所有字符都是小写
+print(str.isupper()) # 判断所有字符都是大写
+print(str.istitle()) # 判断所有单词都是首字母大写，像标题
+print(str.isspace()) # 判断所有字符都是空白字符、\t、\n、\r
+
+print('-' * 20)
+
+# 测试实例二
+print("测试实例二")
+test = "runoob"
+print(test.isalnum())
+print(test.isalpha())
+print(test.isdigit())
+print(test.islower())
+print(test.isupper())
+print(test.istitle())
+print(test.isspace())
