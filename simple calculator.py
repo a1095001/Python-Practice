7b22c8d9b2d382232ed3b76bcd939489951fa44e
a1095001 <KeymanWu0@hotmail.com>
2020-02-08 10:09:23 +0800

    简单计算器

--- simple calculator.py
+++ simple calculator.py
@@ -13,16 +13,17 @@ def GetDivision(x , y):
     else:
         return x / y
 
-def switcher(value):
-    switcher = {
+def switcher(value , x , y):
+    mode = {
         0: 'GetPlus',
         1: 'GetSubtraction',
         2: 'GetMultiplication',
         3: 'GetDivision',
     }
-    return switcher.get(value , 'wrong value')
+    return mode.get(value)(x , y)
 
 choice = int(input('请选择模式：\n1:加法\n2:减法\n3:乘法\n4:除法\n'))
 x , y = map(int, input('请输入要进行计算的两个数字：').split(' '))
-result = switcher.get(x , y)
+print(isinstance(x,int))
+result = switcher(int(choice), x , y)
 print(f'{x}和{y}的运算结果为:{result}')
