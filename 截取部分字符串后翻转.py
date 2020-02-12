c89bf49cb4f911bf3255f5a03b16d94cc0bfb940
s-keyman <KeymanWu0@hotmail.com>
2020-02-12 23:47:25 +0800

    截取部分字符串后翻转

---
+++ 截取部分字符串后翻转.py
@@ -0,0 +1,12 @@
+str_1 = "www.s-keyman.site"
+def rotate(input,d): 
+    # 去掉头d个字符串
+    Lfirst = input[:d]
+    # 截取从d到末尾的字符串
+    Lsecond = input[d:]
+    Rfirst = input[:len(input)-d] 
+    Rsecond = input[len(input)-d:] 
+    print( "头部切片翻转 : ", (Lsecond + Lfirst) )
+    print( "尾部切片翻转 : ", (Rsecond + Rfirst) )
+
+print(rotate(str_1, 4))
