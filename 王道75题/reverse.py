4f1df8ed38174ec5aa27b498490f89037531aaa9
s-keyman <KeymanWu0@hotmail.com>
2020-02-16 21:40:16 +0800

    New way to solve the problem

--- 王道75题/reverse.py
+++ 王道75题/reverse.py
@@ -1,3 +1,4 @@
+import numpy
 num = int(input('list的元素个数：'))
 '''
 # 倒序遍历
@@ -17,3 +18,14 @@ print('翻转前：\n', array, '\n' '翻转后：\n', array_reverse)
 array = [num for num in range(1, num+1)]
 array_reverse = array[::-1]
 print('翻转前：\n', array, '\n' '翻转后：\n', array_reverse)
+
+# 新算法
+def Reverse(list):
+    for i in range(len(list)//2):
+        list[i], list[len(list)-1-i] = \
+            list[len(list)-1-i], list[i]
+    return list
+        
+arr = list(numpy.random.randint(1, 100, num))
+print('翻转前：\n', arr)
+print('翻转后：\n', Reverse(arr))
