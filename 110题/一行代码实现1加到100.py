b87b71e9a1cb71a8dbec3552778404fb834442e1
s-keyman <KeymanWu0@hotmail.com>
2020-02-24 22:06:23 +0800

    123

--- 110题/一行代码实现1加到100.py
+++ 110题/一行代码实现1加到100.py
@@ -1,2 +1,1 @@
 print("结果为：", sum(range(1, 101)))
-int float str list tuple dict bool complex
--- 王道75题/sum_of_Series.py
+++ 王道75题/sum_of_Series.py
@@ -1,8 +1,8 @@
 # 使用循环实现计算 n 个自然数的立方和
 def sum_of_series(num):
-    sum = 0
+    summery = 0
     for i in range(1, num+1):
-        sum = sum + i**3
+        summery = summery + i**3
         pass
-    return sum
+    return summery
 print(sum_of_series(100))
